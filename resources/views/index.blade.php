5d8cbbd85be8d183303012d56b0e8fac8a41bdba
josephkristanto10 <josephkristanto10@gmail.com>
2024-01-17 10:35:32 +0700

    update 17 jan 2024

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -221,10 +221,7 @@ color: #ff5A00;
 							</div>
 						</div>
 						<div class="video-wrap">
-							{{-- <video id="slide-video" poster="{{ asset('css/images/videos/1.jpg') }}" preload="auto" loop autoplay muted> --}}
-								{{-- <source src='{{ asset('css/images/videos/1.webm') }}' type='video/webm' /> --}}
-								{{-- <source src='{{ asset('css/images/videos/1.mp4') }}' type='video/mp4' /> --}}
-							{{-- </video> --}}
+						
 							<div class="video-overlay" style="background-color: rgba(0,0,0,0.1);"></div>
 						</div>
 					</div>
